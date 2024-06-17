0d1f1c86c27024e7c4850bc2ba0c9137bbf5402a
OluC94 <coker.olu94@gmail.com>
2024-06-17 15:52:50 +0100

    complete implementation of fizzbuzz challenge

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { generateFizzBuzz } from './fizzBuzz'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fizzBuzz, setFizzBuzz] = useState(0)
+  const [displayedFB, setDisplayedFB] = useState(0);
+
+  const handleUpdateFizzBuzz = () => {
+    setFizzBuzz(currVal => currVal + 1)
+    setDisplayedFB(generateFizzBuzz(fizzBuzz + 1))
+  }
+
+  const handleResetFizzBuzz = () => {
+    setFizzBuzz(0)
+    setDisplayedFB(0)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>{displayedFB}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={handleUpdateFizzBuzz}>
+          Next
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+        <button onClick={handleResetFizzBuzz}>
+          Reset
+        </button>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
